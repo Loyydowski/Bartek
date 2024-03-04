f5544d2f9fcc2011610441958e1f41e07df1f4b7
bkubazst <87015274+Loyydowski@users.noreply.github.com>
2024-03-04 14:25:05 +0100

    Update AI_PLUGIN.js

--- AI_PLUGIN.js
+++ AI_PLUGIN.js
@@ -1877,7 +1877,6 @@
         }
 
         function closeDragElement() {
-            // stop moving when mouse button is released:
             document.removeEventListener("mouseup", closeDragElement);
             document.removeEventListener("touchend", closeDragElement);
             document.removeEventListener("mousemove", elementDrag);
@@ -1885,15 +1884,6 @@
         }
     }
 
-
-
-
-
-
-
-
-
-    // Source: https://github.com/hughsk/png-chunks-extract
     var uint8 = new Uint8Array(4)
     var int32 = new Int32Array(uint8.buffer)
     var uint32 = new Uint32Array(uint8.buffer)
@@ -1912,22 +1902,17 @@
         var idx = 8
 
         while (idx < data.length) {
-            // Read the length of the current chunk,
-            // which is stored as a Uint32.
+
             uint8[3] = data[idx++]
             uint8[2] = data[idx++]
             uint8[1] = data[idx++]
             uint8[0] = data[idx++]
-
-            // Chunk includes name/type for CRC check (see below).
             var length = uint32[0] + 4
             var chunk = new Uint8Array(length)
             chunk[0] = data[idx++]
             chunk[1] = data[idx++]
             chunk[2] = data[idx++]
             chunk[3] = data[idx++]
-
-            // Get the name in ASCII for identification.
             var name = (
                 String.fromCharCode(chunk[0]) +
                 String.fromCharCode(chunk[1]) +
@@ -1935,13 +1920,10 @@
                 String.fromCharCode(chunk[3])
             )
 
-            // The IHDR header MUST come first.
             if (!chunks.length && name !== 'IHDR') {
                 throw new Error('IHDR header missing')
             }
 
-            // The IEND header marks the end of the file,
-            // so on discovering it break out of the loop.
             if (name === 'IEND') {
                 ended = true
                 chunks.push({
@@ -1952,13 +1934,10 @@
                 break
             }
 
-            // Read the contents of the chunk out of the main buffer.
             for (var i = 4; i < length; i++) {
                 chunk[i] = data[idx++]
             }
 
-            // Read out the CRC value for comparison.
-            // It's stored as an Int32.
             uint8[3] = data[idx++]
             uint8[2] = data[idx++]
             uint8[1] = data[idx++]
@@ -1972,8 +1951,6 @@
                 )
             }
 
-            // The chunk data is now copied to remove the 4 preceding
-            // bytes used for the chunk name/type.
             var chunkData = new Uint8Array(chunk.buffer.slice(4))
 
             chunks.push({
